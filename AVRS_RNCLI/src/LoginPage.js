a92af02dfa828e0c4ffeec7e7bf0947d07f4e74b
arkbuilder117 <noah.matwalker@gmail.com>
2022-07-14 12:56:00 -0500

    Cleaned up code using prettier

--- AVRS_RNCLI/src/LoginPage.js
+++ AVRS_RNCLI/src/LoginPage.js
@@ -1,96 +1,93 @@
-import React, { useState } from 'react';
-import { auth } from '../firebase';
-import { signInWithEmailAndPassword } from "firebase/auth";
+/* eslint-disable no-alert */
+import React, {useState} from 'react';
+import {auth} from '../firebase';
+import {signInWithEmailAndPassword} from 'firebase/auth';
 
-import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import {View, Text, TextInput, Pressable, StyleSheet} from 'react-native';
 
-const LoginPage = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const LoginPage = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSignIn = () => {
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                console.log("LoginPage " + userCredential.user.email);
-                props.logInChange();
-            })
-            .catch(error => alert(error.message))
-    };
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        console.log('LoginPage ' + userCredential.user.email);
+        props.logInChange();
+      })
+      .catch(error => alert(error.message));
+  };
 
-    return (
-        <View>
-            <TextInput
-                style={styles.phoneInput}
-                value={email}
-                onChangeText={setEmail}
-                keyboardType='email-address'
-                placeholder='Email'
-                autoCapitalize='none'
-                autoFocus= {true}
-            />
+  return (
+    <View>
+      <TextInput
+        style={styles.phoneInput}
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        placeholder="Email"
+        autoCapitalize="none"
+        autoFocus={true}
+      />
 
-            <TextInput
-                style={styles.phoneInput}
-                value={password}
-                onChangeText={setPassword}
-                keyboardType='default'
-                placeholder='Password'
-                // placeholderTextColor={'dimgray'}
-                secureTextEntry
-            />
+      <TextInput
+        style={styles.phoneInput}
+        value={password}
+        onChangeText={setPassword}
+        keyboardType="default"
+        placeholder="Password"
+        // placeholderTextColor={'dimgray'}
+        secureTextEntry
+      />
 
-            <Pressable
-                style={({ pressed }) => [
-                    styles.button,
-                    pressed ? { opacity: 0.8 } : {},
-                ]}
-                onPress={handleSignIn}>
-                <Text style={styles.buttonText}>Sign in</Text>
-            </Pressable>
-        </View>
-    )
-}
+      <Pressable
+        style={({pressed}) => [styles.button, pressed ? {opacity: 0.8} : {}]}
+        onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    phoneInput: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#B6C7CC',
-        width: 200,
-        height: 50,
-        borderRadius: 10,
-        margin: 15,
-        padding: 10,
-        bottom: 100,
-    },
-    verify: {
-        width: 200,
-        height: 50,
-        backgroundColor: '#05111B',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        margin: 15,
-    },
-    button: {
-        width: 200,
-        height: 50,
-        backgroundColor: '#05111B',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        margin: 15,
-    },
-    buttonText: {
-        color: '#DBDDDF',
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  phoneInput: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#B6C7CC',
+    width: 200,
+    height: 50,
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    bottom: 100,
+  },
+  verify: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#05111B',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 15,
+  },
+  button: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#05111B',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 15,
+  },
+  buttonText: {
+    color: '#DBDDDF',
+  },
+});
 
 export default LoginPage;
--- AVRS_RNCLI/src/MainMenu.js
+++ AVRS_RNCLI/src/MainMenu.js
@@ -1,69 +1,64 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 import NavBar from './NavBar';
 import MenuPage from './MenuPage';
 import SettingsPage from './SettingsPage';
 import ShoppingCartPage from './ShoppingCartPage';
 
-const MainMenu = (props) => {
-	const [screen, setScreen] = useState('MENU');
-	const [cart, setCart] = useState([]);
+const MainMenu = props => {
+  const [screen, setScreen] = useState('MENU');
+  const [cart, setCart] = useState([]);
 
-	return (
-		<View style={styles.container}>
-			<View style={styles.screenContainer}>
-				{screen === 'MENU' && (
-					<MenuPage setOrder={setCart} curOrder={cart}/>
-				)}
-				{screen === 'LOCATION' && (
-					<Text>Location page</Text>
-				)}
-				{screen === 'CART' && (
-					<ShoppingCartPage cart={cart} setCart={setCart}/>
-				)}
-				{screen === 'SETTINGS' && (
-					<SettingsPage logInChange={props.logInChange}/>
-				)}
-			</View>
-			<View style={styles.navBarContainer}>
-				{/* <Text>Hello world</Text> */}
-				<NavBar screenChange={setScreen}/>
-			</View>
-		</View>
-	)
-}
+  return (
+    <View style={styles.container}>
+      <View style={styles.screenContainer}>
+        {screen === 'MENU' && <MenuPage setOrder={setCart} curOrder={cart} />}
+        {screen === 'LOCATION' && <Text>Location page</Text>}
+        {screen === 'CART' && (
+          <ShoppingCartPage cart={cart} setCart={setCart} />
+        )}
+        {screen === 'SETTINGS' && (
+          <SettingsPage logInChange={props.logInChange} />
+        )}
+      </View>
+      <View style={styles.navBarContainer}>
+        {/* <Text>Hello world</Text> */}
+        <NavBar screenChange={setScreen} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: '#EFF1F7'
-	},
-	screenContainer: {
-		flex: 9,
-		justifyContent: 'center',
-		alignItems: 'center',
-		width: '100%',
-		// backgroundColor: 'orange',
-	},
-	navBarContainer: {
-		flex: 1,
-		// position: 'absolute',
-		justifyContent: 'center',
-		alignItems: 'center',
-		// backgroundColor: 'purple',
-		width: '100%',
-		// bottom: 70,
-	},
-	navBar: {
-		flexDirection: 'row',
-		width: '100%',
-		justifyContent: 'space-evenly',
-	}
-
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#EFF1F7',
+  },
+  screenContainer: {
+    flex: 9,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    // backgroundColor: 'orange',
+  },
+  navBarContainer: {
+    flex: 1,
+    // position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'purple',
+    width: '100%',
+    // bottom: 70,
+  },
+  navBar: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-evenly',
+  },
+});
 
 export default MainMenu;
